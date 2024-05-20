1703817452aeac6eabb97b03be6399a27f321569
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-05-20 20:46:04 +0600

    create product interface

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import path from "path";
 //setup environment variable
 dotenv.config({ path: path.join(process.cwd(), ".env") });
-
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
